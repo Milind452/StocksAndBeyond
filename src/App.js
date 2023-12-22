5d94c1584b07a1ef6102ef8787143bd321b843d1
Milind452 <milind.mohapatra@gmail.com>
2023-12-22 14:42:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+import Optin from "./components/Optin/Optin";
+
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>
+                    Website
+                    <br />
+                    Coming Soon
+                </h1>
+                <Timer />
+                <Optin />
+                <Preloader />
+            </div>
+        </div>
+    );
+}
+
+export default App;
